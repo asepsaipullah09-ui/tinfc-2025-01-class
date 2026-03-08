f030e9294ccddf813a8b28b25b5fad06c0b786f9
Asep <asep.saipullah09@gmail.com>
2026-03-08 21:29:46 +0700

    feat: add sidebar navigation links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,13 +5,32 @@ export default function Sidebar() {
     <aside className="w-64 h-screen bg-gray-900 text-white p-5">
       <h2 className="text-xl font-bold mb-6">TINFC-2025-01</h2>
 
-      <ul className="space-y-4">
-        <li><Link href="/dashboard">Dashboard</Link></li>
-        <li><Link href="/materi">Materi</Link></li>
-        <li><Link href="/tugas">Tugas</Link></li>
-        <li><Link href="/kalender">Kalender</Link></li>
-        <li><Link href="/galeri">Galeri</Link></li>
-        <li><Link href="/mahasiswa">Mahasiswa</Link></li>
+      <ul className="space-y-3">
+
+        <li>
+          <Link href="/dashboard">Dashboard</Link>
+        </li>
+
+        <li>
+          <Link href="/materi">Materi</Link>
+        </li>
+
+        <li>
+          <Link href="/tugas">Tugas</Link>
+        </li>
+
+        <li>
+          <Link href="/kalender">Kalender</Link>
+        </li>
+
+        <li>
+          <Link href="/galeri">Galeri</Link>
+        </li>
+
+        <li>
+          <Link href="/mahasiswa">Mahasiswa</Link>
+        </li>
+
       </ul>
     </aside>
   );
